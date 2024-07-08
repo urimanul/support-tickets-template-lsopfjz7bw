3e96672c55e8a21c71f3c2b35ac90656c752f852
urimanul <myopera9@gmail.com>
2024-07-08 15:19:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,10 @@ SELECT Task_Subject FROM todo_tasks ORDER BY task_ID limit 20;
 
 cur.execute(query__for_fetching)
 
-issue_descriptions = []
+issue_descriptions1 = []
 for row in cur:
     #st.write(row)
-    issue_descriptions.append(row)
+    issue_descriptions1.append(row)
 
 #for fetched_line in cur.fetchall():
     #id = fetched_line['Task_ID']
@@ -56,7 +56,7 @@ if "df" not in st.session_state:
     np.random.seed(42)
 
     # Make up some fake issue descriptions.
-    issue_descriptions1 = [
+    issue_descriptions = [
         "オフィスのネットワーク接続の問題",
         "ソフトウェアアプリケーションが起動時にクラッシュする",
         "プリンターが印刷コマンドに応答しない",
