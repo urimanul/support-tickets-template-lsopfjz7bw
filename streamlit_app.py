ae9adcd6f90241b3c9516063cf7c601bdd6ae67d
urimanul <myopera9@gmail.com>
2024-07-08 15:08:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,23 +30,24 @@ conn = mysql.connector.connect(
 if not conn.is_connected():
     raise Exception("MySQLサーバへの接続に失敗しました")
 
-cur = conn.cursor(dictionary=True)  # 取得結果を辞書型で扱う設定
-#cur = conn.cursor()
+#cur = conn.cursor(dictionary=True)  # 取得結果を辞書型で扱う設定
+cur = conn.cursor()
 
 query__for_fetching = """
-SELECT Task_ID,Task_Subject FROM todo_tasks ORDER BY task_ID;
+SELECT Task_Subject FROM todo_tasks ORDER BY task_ID;
 """
 
 cur.execute(query__for_fetching)
 
-#issue_descriptions = []
-#for row in cursor:
-    #issue_descriptions.append(row)
+issue_descriptions1 = []
+for row in cursor:
+    st.write(row)
+    issue_descriptions1.append(row)
 
-for fetched_line in cur.fetchall():
-    id = fetched_line['Task_ID']
-    name = fetched_line['Task_Subject']
-    st.write(f'{id}: {name}')
+#for fetched_line in cur.fetchall():
+    #id = fetched_line['Task_ID']
+    #name = fetched_line['Task_Subject']
+    #st.write(f'{id}: {name}')
 
 # Create a random Pandas dataframe with existing tickets.
 if "df" not in st.session_state:
