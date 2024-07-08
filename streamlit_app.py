f470b92a25615dfcc93d8777a9782c9cad97bdd0
urimanul <myopera9@gmail.com>
2024-07-08 19:55:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,7 +191,7 @@ st.header("統計")
 
 # Show metrics side by side using `st.columns` and `st.metric`.
 col1, col2, col3 = st.columns(3)
-num_open_tickets = len(st.session_state.df[st.session_state.df.Status == "Open"])
+num_open_tickets = len(st.session_state.df[st.session_state.df.Status == "In Progress"])
 col1.metric(label="オープンチケット数", value=num_open_tickets, delta=10)
 col2.metric(label="レスポンスタイム（時間）", value=5.2, delta=-1.5)
 col3.metric(label="平均時間", value=16, delta=2)
