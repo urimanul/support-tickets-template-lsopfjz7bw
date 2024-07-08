39754de041a25f07539dfc35486a791b9c753891
urimanul <myopera9@gmail.com>
2024-07-08 19:19:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -126,7 +126,8 @@ if submitted:
     # Make a dataframe for the new ticket and append it to the dataframe in session
     # state.
 
-    recent_ticket_number = int(lastID)+1
+    recent_ticket_number = len(data1)+1
+    st.write(recent_ticket_number)
     #recent_ticket_number = int(max(st.session_state.df.ID).split("-")[1])
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     df_new = pd.DataFrame(
